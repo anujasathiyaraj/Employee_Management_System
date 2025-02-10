e17ce12ba8af74179cd5b87db4979a475dcb8ae4
anujasathiyaraj <96020465+anujasathiyaraj@users.noreply.github.com>
2025-02-10 08:52:36 +0530

    Update Employee.java

--- ems/src/main/java/com/example/ems/entity/Employee.java
+++ ems/src/main/java/com/example/ems/entity/Employee.java
@@ -7,6 +7,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "employee")
 public class Employee {
     @Id
@@ -19,46 +23,4 @@ public class Employee {
     @Column(name = "email_id",nullable = false,unique = true)
     private String email;
 
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Employee(Long id, String firstName, String lastName, String email) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-    }
-
-    public Employee() {
-    }
 }
